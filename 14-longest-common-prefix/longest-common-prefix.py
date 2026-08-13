5407b55f8123dc61772e21d72dbb81e8ce4fef5f
Gahan-Shetty <gahanshetty16@gmail.com>
2026-08-13 22:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -8,7 +8,7 @@ class Solution:
         for i in strs[1:]:
             while not i.startswith(prefix):
                 prefix=prefix[:-1]
-            if not  strs:
+            if not  prefix:
                 return ""
         return prefix
 
