6b6fe628622de1475baa6379a30c92783add7796
Gahan-Shetty <gahanshetty16@gmail.com>
2026-08-18 21:41:20 +0530

    Time: 22 ms (73.86%) | Memory: 54.1 MB (48.76%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,19 +1,28 @@
 class Solution {
     public int equalPairs(int[][] grid) {
+        HashMap<List<Integer>,Integer>ans=new HashMap<>();
         int count=0;
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid.length;j++){
-                boolean equal=true;
-                for(int k=0;k<grid.length;k++){
-                    if(grid[i][k]!=grid[k][j]){
-                    equal=false;
-                    break;
-                }
-                }
-                if(equal)
-                count++;
+            List<Integer>row=new ArrayList<>();
+            for(int k = 0; k < grid.length; k++) {
+            row.add(grid[i][k]);
+             }
+            if(ans.containsKey(row))
+            ans.put(row,ans.get(row)+1);
+            else
+            ans.put(row,1);
         }
+
+            for(int j=0;j< grid.length;j++){
+            List<Integer>col=new ArrayList<>();
+             for(int k = 0; k < grid.length; k++) {
+            col.add(grid[k][j]);
+             }
+            if(ans.containsKey(col))
+            count+= ans.get(col);
+            else
+            continue;
+        }
+        return count;
     }
-    return count;
-}
 }
